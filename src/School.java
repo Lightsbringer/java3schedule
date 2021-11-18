63b3c977f6faaf71f5833381ebcc5031a15dae2d
Luke^^ <lyuboslav.lesichkov@student.nhlstenden.com>
2021-11-18 09:36:20 +0100

    Update School.java

--- src/School.java
+++ src/School.java
@@ -1,2 +1,10 @@
 public class School {
+    private String name;
+    private HashSet<Education> educations;
+    public School(String name)
+    {
+        this.name = name;
+        HashSet<Education> educations = new HashSet<>;
+        
+    }
 }
