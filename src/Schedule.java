1baf7c1e31b88a8a1e5a42040866997660aa6193
Luke^^ <lyuboslav.lesichkov@student.nhlstenden.com>
2021-11-18 09:33:29 +0100

    Update Schedule.java

--- src/Schedule.java
+++ src/Schedule.java
@@ -1,2 +1,17 @@
 public class Schedule {
+    private ArrayList<String> event;
+    public Schedule()
+    {
+        ArrayList<String> event = new ArrayList<String>();
+    }
+
+    public ArrayList<String> getEvent()
+    {
+        return event;
+    }
+
+    public void setEvent(ArrayList<String> event)
+    {
+        this.event = event;
+    }
 }
